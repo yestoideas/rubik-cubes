fced30b57a950577819714f1ccc4b1a7054b235a
developerashikur1 <masik4506@gmail.com>
2024-04-16 22:59:16 +0600

    cube front side data updated.

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
 import { Box, Environment, Html, OrbitControls, RenderTexture, Stats, Text } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import TWEEN from '@tweenjs/tween.js'
@@ -72,24 +74,29 @@ function SingleCube({ position, geometry, onClick }) {
     )
 }
 
-function Cube({setStep}) {
+function Cube({ setStep }) {
     const ref = useRef();
     const roundedBoxGeometry = useMemo(() => {
         return new RoundedBoxGeometry(1, 1, 1, 1, 0.05)
-    }, [])
+    }, []);
+
+    // console.log("RoundedBoxGeometry", roundedBoxGeometry)
 
     const [showSingleCube, setShowSingleCube] = useState(false)
-    const [singleCubePosition, setSingleCubePosition] = useState([0, 0, 0])
+    const [singleCubePosition, setSingleCubePosition] = useState([0, 0, 0]);
+
 
     const [dbClickTimer, setDbClickTimer] = useState(null);
 
-    const handleCubeletClick = (position) => {
+    const handleCubeletClick = (position, exactPosition) => {
+
+        console.log("exactPosition", exactPosition);
+        // setSingleCubePosition(position);
         if (dbClickTimer) {
             clearTimeout(dbClickTimer);
             setDbClickTimer(null);
 
             // setShowSingleCube(!showSingleCube);
-            setSingleCubePosition(position);
 
             setStep('secondtLayer')
         } else {
@@ -120,30 +127,34 @@ function Cube({setStep}) {
                     onClick={() => handleCubeletClick(singleCubePosition)}
                 />
             ) : ( */}
-                <group ref={ref}>
-                    {[...Array(3).keys()].map((x) =>
-                        [...Array(3).keys()].map((y) =>
-                            [...Array(3).keys()].map((z) => (
-                                <Cubelet
-                                    key={x + y * 3 + z * 9}
-                                    position={[x - 1, y - 1, z - 1]}
-                                    geometry={roundedBoxGeometry}
-                                    onClick={() => handleCubeletClick([x - 1, y - 1, z - 1])}
-                                />
-                            ))
-                        )
-                    )}
-                </group>
+            <group ref={ref}>
+                {[...Array(3).keys()].map((x, i) =>
+                    [...Array(3).keys()].map((y, j) =>
+                        [...Array(3).keys()].map((z, k) => (
+                            <Cubelet
+                                indexes={[i, j, k]}
+                                positionss={[x, y, z]}
+                                key={x + y * 3 + z * 9}
+                                position={[x - 1, y - 1, z - 1]}
+                                geometry={roundedBoxGeometry}
+                                onClick={() => handleCubeletClick([x - 1, y - 1, z - 1], [x, y, z])}
+                            />
+                        ))
+                    )
+                )}
+            </group>
             {/* )} */}
         </>
     )
 }
 
-function Cubelet({ position, geometry, onClick }) {
+function Cubelet({ position, geometry, onClick, indexes, positionss }) {
+    // console.log('position', positionss, "indexes", indexes);
+    // console.log('position', position, "indexes", indexes);
 
 
     return (
-        
+
         <mesh position={position} geometry={geometry} onClick={onClick} scale={[1, 1, 1]}>
             {[...Array(6).keys()].map((i) => (
                 <meshStandardMaterial key={i} attach={`material-${i}`} />
@@ -151,7 +162,7 @@ function Cubelet({ position, geometry, onClick }) {
             {/* <Html occlude distanceFactor={3.5} position={[0, 0, 0.51]} transform>
         <span>xc1</span>
       </Html> */}
-       {/* <Text
+            {/* <Text
         position={[-1, 0.5, 0.5]} // Adjust for each face
         fontSize={0.1}
         anchorX="center"
@@ -161,7 +172,7 @@ function Cubelet({ position, geometry, onClick }) {
       >
         Bangladesh
       </Text> */}
-       {/* <Text
+            {/* <Text
         position={[0.5, 0.5, 0.1]} // Adjust for each face
         fontSize={0.1}
         anchorX="center"
@@ -171,19 +182,19 @@ function Cubelet({ position, geometry, onClick }) {
       >
         Bangladesh
       </Text> */}
-      {/* <boxGeometry args={[1, 1, 1]} />
+            {/* <boxGeometry args={[1, 1, 1]} />
       */}
 
-                {/* <meshStandardMaterial color="white" />
+            {/* <meshStandardMaterial color="white" />
       <CubeLabel text="Front" position={[0, 0, 0.51]} rotation={[0, 0, 0]} />
       <CubeLabel text="Back" position={[0, 0, -0.51]} rotation={[0, Math.PI, 0]} />
       <CubeLabel text="Top" position={[0, 0.51, 0]} rotation={[Math.PI / 2, 0, 0]} />
       <CubeLabel text="Bottom" position={[0, -0.51, 0]} rotation={[-Math.PI / 2, 0, 0]} />
       <CubeLabel text="Left" position={[-0.51, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
       <CubeLabel text="Right" position={[0.51, 0, 0]} rotation={[0, Math.PI / 2, 0]} />  */}
-        {/* <meshStandardMaterial color="white" />
+            {/* <meshStandardMaterial color="white" />
       <CubeLabel text="Front" position={[-3, 0, 0.51]} rotation={[0, 0, 0]} /> */}
-      {/* <CubeLabel text="Back" position={[0, 0, -0.51]} rotation={[0, Math.PI, 0]} />
+            {/* <CubeLabel text="Back" position={[0, 0, -0.51]} rotation={[0, Math.PI, 0]} />
       <CubeLabel text="Top" position={[0, 0.51, 0]} rotation={[Math.PI / 2, 0, 0]} />
       <CubeLabel text="Bottom" position={[0, -0.51, 0]} rotation={[-Math.PI / 2, 0, 0]} />
       <CubeLabel text="Left" position={[-0.51, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
@@ -191,68 +202,144 @@ function Cubelet({ position, geometry, onClick }) {
         </mesh>
     )
 }
+const colorGrades = {
+    XA1:[-8,-5,-3,-2,-1,2,0,3,1,-4,1],
+    XB1:[-7,-4,-2,-1,0,3,1,4,2,-2,2],
+    XC1:[-6,-3,-1,0,1,4,2,5,3,-1,3],
+    YA1:[-6,-3,-1,0,1,3,2,4,2,2,3],
+    YB1:[-5,-2,0,1,2,4,3,5,3,3,4],
+    YC1:[-4,-1,1,2,3,5,4,6,4,4,5],
+    ZA1:[-8,-4,-2,-1,0,2,1,3,1,-3,2],
+    ZB1:[-7,-3,-1,0,1,3,2,4,2,-1,3],
+    ZC1:[-6,-2,0,1,2,4,3,5,3,0,4],
+};
 
 
 
 const cubeData = [
+    // Front side data
     {
-      text: 'Solutions',
-      anchorX: 'right',
-      position: [-1.8, 1, 1.5],
-      rotation: [0, 0, 0],
+        text: 'XC1',
+        anchorX: 'center',
+        position: [-1, 1, 1.5],
+        rotation: [0, 0, 0],
     },
     {
-      text: 'Transition',
-      anchorX: 'right',
-      position: [-1.8, 0, 1.5],
-      rotation: [0, 0, 0],
+        text: 'XB1',
+        anchorX: 'center',
+        position: [-1, 0, 1.5],
+        rotation: [0, 0, 0],
     },
     {
-      text: 'texts',
-      anchorX: 'right',
-      position: [-1.8, -1, 1.5],
-      rotation: [0, 0, 0],
+        text: 'XA1',
+        anchorX: 'center',
+        position: [-1, -1, 1.5],
+        rotation: [0, 0, 0],
     },
-    {
-      text: 'Climate\nmitigation',
-      anchorX: 'center',
-      position: [1, -1.7, 1.5],
-      rotation: [0, 0, 0],
-    },
-    {
-      text: 'Climate\nadaptation',
-      anchorX: 'center',
-      position: [0, -1.7, 1.5],
-      rotation: [0, 0, 0],
-    },
-    {
-      text: 'Biodiversity',
-      anchorX: 'center',
-      position: [-1, -1.7, 1.5],
-      rotation: [0, 0, 0],
-    },
-    {
-      text: 'Spend',
-      anchorX: 'center',
-      position: [2, -1.7, -1],
-      rotation: [0, 0, -0.31],
-    },
-    {
-      text: 'Lend',
-      anchorX: 'center',
-      position: [2, -1.7, 0],
-      rotation: [0, 0, -0.31],
-    },
-    {
-      text: 'Invest',
-      anchorX: 'center',
-      position: [2, -1.7, 1],
-      rotation: [0, 0, -0.31],
-    },
-  ];
-  
 
-function RubikCube({setStep}) {
+    {
+        text: 'YC1',
+        anchorX: 'center',
+        position: [0, 1, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'YB1',
+        anchorX: 'center',
+        position: [0, 0, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'YA1',
+        anchorX: 'center',
+        position: [0, -1, 1.5],
+        rotation: [0, 0, 0],
+    },
+
+    {
+        text: 'ZC1',
+        anchorX: 'center',
+        position: [1, 1, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'ZB1',
+        anchorX: 'center',
+        position: [1, 0, 1.5],
+        rotation: [0, 0, 0],
+    },
+
+    {
+        text: 'ZA1',
+        anchorX: 'center',
+        position: [1, -1, 1.5],
+        rotation: [0, 0, 0],
+    },
+
+
+    // Left side data
+    {
+        text: 'C:\nSolutions',
+        anchorX: 'right',
+        position: [-1.8, 1, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'B:\nTransition',
+        anchorX: 'right',
+        position: [-1.8, 0, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'A:\nRisk\nreduction',
+        anchorX: 'right',
+        position: [-1.8, -1, 1.5],
+        rotation: [0, 0, 0],
+    },
+
+    // Front-Down side data
+    {
+        text: 'X: Climate\nmitigation',
+        anchorX: 'center',
+        position: [-1, -1.7, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'Y: Climate\nadaptation',
+        anchorX: 'center',
+        position: [0, -1.7, 1.5],
+        rotation: [0, 0, 0],
+    },
+    {
+        text: 'Z: Biodiversity',
+        anchorX: 'center',
+        position: [1, -1.7, 1.5],
+        rotation: [0, 0, 0],
+    },
+
+    // Right-Down side data
+    {
+        text: '3:\nSpend',
+        anchorX: 'center',
+        position: [2, -1.7, -1],
+        rotation: [0, 0, -0.31],
+    },
+    {
+        text: '2:\nLend',
+        anchorX: 'center',
+        position: [2, -1.7, 0],
+        rotation: [0, 0, -0.31],
+    },
+    {
+        text: '1:\nInvest',
+        anchorX: 'center',
+        position: [2, -1.7, 1],
+        rotation: [0, 0, -0.31],
+    },
+];
+
+
+function RubikCube({ setStep }) {
     const texts = 'Risk\nreduction';
     return (
         <>
@@ -275,13 +362,13 @@ function RubikCube({setStep}) {
                 <Box />
                 <meshStandardMaterial color="white" />
                 <>
-                {/* left */}
+                    {/* left */}
                 </>
                 <>
-                {/* bottom */}
+                    {/* bottom */}
                 </>
                 <>
-                {/* right */}
+                    {/* right */}
                     {/* <CubeLabel anchorX={'right'} text="Solutions" position={[-1.8, 1, 1.5]} rotation={[0, 0, 0]} />
                     <CubeLabel anchorX={'right'} text="Transition" position={[-1.8, 0, 1.5]} rotation={[0, 0, 0]} />
                     <CubeLabel anchorX={'right'} text={texts} position={[-1.8, -1, 1.5]} rotation={[0, 0, 0]} />
@@ -296,14 +383,14 @@ function RubikCube({setStep}) {
 
                 </>
                 {cubeData.map((cube, index) => (
-        <CubeLabel
-          key={index}
-          anchorX={cube.anchorX}
-          text={cube.text}
-          position={cube.position}
-          rotation={cube.rotation}
-        />
-      ))}
+                    <CubeLabel
+                        key={index}
+                        anchorX={cube.anchorX}
+                        text={cube.text}
+                        position={cube.position}
+                        rotation={cube.rotation}
+                    />
+                ))}
             </Canvas>
         </>
     )
@@ -313,18 +400,18 @@ export default RubikCube;
 
 const CubeLabel = ({ text, position, rotation, anchorX }) => {
     return (
-      <Text
-        position={position}
-        rotation={rotation}
-        fontSize={0.12}
-        color="black"
-        anchorX={anchorX}
-        anchorY="middle"
-        remove={true}
-       whiteSpace='pre-wrap' 
-      >
-        {text}
-      </Text>
+        <Text
+            position={position}
+            rotation={rotation}
+            fontSize={0.12}
+            color="black"
+            anchorX={anchorX}
+            anchorY="middle"
+            remove={true}
+            whiteSpace='pre-wrap'
+        >
+            {text}
+        </Text>
     )
-  }
+}
 
