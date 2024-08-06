bcf8897b0cb197b5aeceeddd6344280029cfd5ca
Hritwik Das <32536156+dashritwik@users.noreply.github.com>
2024-08-06 13:15:38 +0530

    feedback changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,34 @@ import SecondLayerLayout from "./SecondLayer";
 import InititalLoadingCube from "./components/InitialLoadingCube";
 
 const theme = createTheme({
-    typography: {
-      fontFamily: ["Playfair Display", "serif"].join(","),
-    },
-  });
-
+  typography: {
+    fontFamily: ["Playfair Display", "serif"].join(","),
+  },
+});
 
 const App = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [step, setStep] = useState('first');
-    const [activeValues, setActiveValues] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [step, setStep] = useState("first");
+  const [activeValues, setActiveValues] = useState({});
 
-    useEffect(() => {
-        setIsLoading(true);
+  //   useEffect(() => {
+  //     setIsLoading(true);
 
-        const timeout = setTimeout(() => {
-            setIsLoading(false);
-        }, 5000);
+  //     const timeout = setTimeout(() => {
+  //       setIsLoading(false);
+  //     }, 5000);
 
-        return () => clearTimeout(timeout);
-    }, []);
+  //     return () => clearTimeout(timeout);
+  //   }, []);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <div style={{ height: "100%", width: '100%' }}>
-                {step === "first" && <SecondLayerLayout activeValues={activeValues} />}
-                <Box sx={{ position: 'absolute', left: '0', top: '0', height: '100%', width: '100%', transition: 'all 2s ease-in-out', visibility: isLoading ? 'visible' : 'hidden', zIndex: 1000 }}><InititalLoadingCube /></Box>
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={{ height: "100%", width: "100%" }}>
+        {step === "first" && <SecondLayerLayout activeValues={activeValues} />}
+        {/* <Box sx={{ position: 'absolute', left: '0', top: '0', height: '100%', width: '100%', transition: 'all 2s ease-in-out', visibility: isLoading ? 'visible' : 'hidden', zIndex: 1000 }}><InititalLoadingCube /></Box> */}
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/SecondLayer/index.jsx
+++ src/SecondLayer/index.jsx
@@ -8,178 +8,234 @@ import { cubeDetails, sectorsDetails } from "../utils/cubeDetails";
 import SecondLayer from "./SecondLayer";
 // import SecondLayer from "./SecondLayer";
 
-
-
-
-
 const SecondLayerLayout = () => {
-    const [open, setOpen] = useState(false);
-    const [analysisData, setAnalysisData] = useState({});
-    const [selectedShortText, setSelectedShortText] = useState('');
-    const [cubeData, setCubeData] = useState();
-    // const [cubeData, setCubeData] = useState([]);
-    const [step, setStep] = useState(0);
-    const [countryStep, setCountryStep] = useState(0);
-    const [selectedCubeData, setSelectedCubeData] = useState([]);
-    const [firstData, setFirstData] = useState({});
-    const [secondData, setSecondData] = useState({});
-    const [thirdData, setThirdData] = useState({});
+  const [open, setOpen] = useState(false);
+  const [analysisData, setAnalysisData] = useState({});
+  const [selectedShortText, setSelectedShortText] = useState("");
+  const [cubeData, setCubeData] = useState();
+  // const [cubeData, setCubeData] = useState([]);
+  const [step, setStep] = useState(0);
+  const [countryStep, setCountryStep] = useState(0);
+  const [selectedCubeData, setSelectedCubeData] = useState([]);
+  const [firstData, setFirstData] = useState({});
+  const [secondData, setSecondData] = useState({});
+  const [thirdData, setThirdData] = useState({});
 
+  console.log(selectedShortText.length);
+  const handleSelectedCube = (payload) => {
+    setSelectedShortText(payload.text);
+    // setFirstData(payload.text[0]);
+    // setSecondData(payload.text[1]);
+    // setThirdData(payload.text[2]);
+    setOpen(true);
+  };
 
-    console.log(selectedShortText.length)
-    const handleSelectedCube = (payload) => {
-        setSelectedShortText(payload.text);
-        // setFirstData(payload.text[0]);
-        // setSecondData(payload.text[1]);
-        // setThirdData(payload.text[2]);
-        setOpen(true);
+  const handleBackSidebar = () => {
+    // setAnalysisData([]);
+    setStep((prev) => prev - 1);
+    if (countryStep > 0) {
+      setCountryStep((prev) => prev - 1);
     }
+  };
 
+  const handleCloseSidebar = () => {
+    setOpen(false);
+    setSelectedCubeData([]);
+    setCountryStep(0);
+  };
 
+  const handleAnalysisData = () => {
+    const newData = [...sectorsDetails].map((sector) => {
+      const sectorData = sector.overviews.filter(
+        (item) => item.title === selectedShortText.slice(0, 2)
+      );
+      return {
+        ...sector,
+        overviews: sectorData[0],
+      };
+    });
 
-    const handleBackSidebar = () => {
-        // setAnalysisData([]);
-        setStep((prev) => prev - 1);
-        if(countryStep > 0){
-            setCountryStep((prev) => prev - 1);
-        }
-    }
-    
-    const handleCloseSidebar = () => {
-        setOpen(false);
-        setSelectedCubeData([]);
-        setCountryStep(0);
-    }
+    console.log(newData);
 
-    const handleAnalysisData = () => {
-        const newData = [...sectorsDetails].map((sector) => {
-            const sectorData = sector.overviews.filter(item => item.title === selectedShortText.slice(0, 2));
-            return {
-                ...sector,
-                overviews: sectorData[0],
-            }
-        });
+    setAnalysisData(newData);
+    setStep((prev) => prev + 1);
+  };
 
-        console.log(newData)
+  const handleCountryData = () => {
+    setCountryStep((prev) => prev + 1);
+  };
 
-        setAnalysisData(newData);
-        setStep((prev) => prev + 1);
-    }
+  const handleSelectAnalysis = (payload) => {
+    // if (!selectedShortText || !selectedShortText.text) {
+    //     console.error('selectedShortText or selectedShortText.text is undefined');
+    //     return;
+    // }
 
-    const handleCountryData = () => {
-        setCountryStep((prev) => prev + 1);
-    }
+    // const sectorData = payload?.overviews?.filter(item => item.title === selectedShortText.text.slice(0, 2));
+    console.log(selectedShortText);
+    // const sectorData = payload?.overviews?.filter(item => item.title === selectedShortText.text.slice(0, 2));
+    // console.log(sectorData)
+    const sectorData = payload?.overviews?.filter(
+      (item) => item.title === selectedShortText.slice(0, 2)
+    );
 
-    const handleSelectAnalysis = (payload) => {
-        // if (!selectedShortText || !selectedShortText.text) {
-        //     console.error('selectedShortText or selectedShortText.text is undefined');
-        //     return;
-        // }
-    
-        // const sectorData = payload?.overviews?.filter(item => item.title === selectedShortText.text.slice(0, 2));
-        console.log(selectedShortText);
-        // const sectorData = payload?.overviews?.filter(item => item.title === selectedShortText.text.slice(0, 2));
-        // console.log(sectorData)
-        const sectorData = payload?.overviews?.filter(item => item.title === selectedShortText.slice(0, 2));
+    setAnalysisData({
+      title: payload.title,
+      overviews: sectorData[0],
+    });
 
-        setAnalysisData({
-            title: payload.title,
-            overviews: sectorData[0],
-        });
+    setStep((prev) => prev + 1);
+  };
 
-        setStep((prev) => prev + 1);
-    }
+  const pageContent = [
+    {
+      type: "html",
+      content: "<p>Welcome to this <strong>page</strong></p>",
+    },
+    {
+      type: "html",
+      content: "<h2>Another Section</h2><p>This is another section.</p>",
+    },
+  ];
 
+  return (
+    <>
+      <Stack direction={"row"} sx={{ height: "100%" }}>
+        {/* left side */}
+        <Stack
+          sx={{
+            height: "100%",
+            width: open ? "60%" : "100%",
+            p: 3,
+            transition: "all 0.3s ease-in-out",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {/* <Button onClick={() => setOpen((prev) => !prev)}> click </Button> */}
+          {/* <SquareBoxes onClickLabel={handleSelectedCube} /> */}
+          {/* <RubikCube setStep={setStep} /> */}
+          {/* <CustomCube /> */}
+          {/* <CustomCube onClickLabel={handleSelectedCube} /> */}
+          <CustomCube2 onClickLabel={handleSelectedCube} />
+        </Stack>
 
-    const pageContent = [
-        {
-          type: 'html',
-          content: '<p>Welcome to this <strong>page</strong></p>',
-        },
-        {
-          type: 'html',
-          content: '<h2>Another Section</h2><p>This is another section.</p>',
-        },
-      ];    
+        {/* right side */}
+        <Stack
+          direction={"column"}
+          spacing={8}
+          sx={{
+            bgcolor: "white",
+            height: "100%",
+            // width: '40%',
+            width: open ? "40%" : "0px",
+            display: open ? "flex" : "none",
+            p: 3,
+            // left: 'auto',
+            // right: '0',
+            overflow: "hidden",
+            overflowY: "auto",
+            // transition: 'width 0.3s ease-out',
+          }}
+        >
+          <Stack
+            direction={"row"}
+            justifyContent={"flex-end"}
+            sx={{ marginBottom: "-2.5rem !important" }}
+          >
+            {/* Object.keys(analysisData).length > 0 ? */}
+            {step !== 0 ? (
+              <IconButton
+                onClick={handleBackSidebar}
+                sx={{ bgcolor: "gray !important", color: "#ffffff" }}
+              >
+                <ArrowBack />{" "}
+              </IconButton>
+            ) : (
+              <IconButton
+                onClick={handleCloseSidebar}
+                sx={{ bgcolor: "gray !important", color: "#ffffff" }}
+              >
+                <Close />{" "}
+              </IconButton>
+            )}
+          </Stack>
 
-    return (
-        <>
-            <Stack direction={'row'} sx={{ height: '100%' }}>
-                {/* left side */}
-                <Stack
-                    sx={{
-                        height: '100%',
-                        width: open ? '60%' : '100%',
-                        p: 3,
-                        transition: 'all 0.3s ease-in-out',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}
-                >
-                    {/* <Button onClick={() => setOpen((prev) => !prev)}> click </Button> */}
-                    {/* <SquareBoxes onClickLabel={handleSelectedCube} /> */}
-                    {/* <RubikCube setStep={setStep} /> */}
-                    {/* <CustomCube /> */}
-                    <CustomCube onClickLabel={handleSelectedCube} />
-                    {/* <CustomCube2 onClickLabel={handleSelectedCube} /> */}
-                </Stack>
+          {/* !Object.keys(analysisData).length > 0 ? */}
+          {countryStep === 0 ? (
+            <>
+              {step === 0 ? (
+                <>
+                  <Stack direction={"row"} alignItems={"center"} spacing={2}>
+                    {selectedShortText?.split("")?.map((item, i) => (
+                      <>
+                        <Typography
+                          lineHeight={1}
+                          key={`${item}`}
+                          variant="h6"
+                          fontWeight={600}
+                        >
+                          {/* {item} */}
+                          {cubeDetails[item]?.title}
+                        </Typography>
 
+                        {i < selectedShortText.length - 1 ? (
+                          <Divider
+                            sx={{
+                              borderWidth: "1px",
+                              height: "100%",
+                              borderColor: "gray",
+                            }}
+                          />
+                        ) : null}
+                      </>
+                    ))}
+                  </Stack>
+                  <Stack
+                    direction={"row"}
+                    spacing={3}
+                    justifyContent={"flex-start"}
+                    sx={{ marginBottom: "" }}
+                  >
+                    {/* {!Object.keys(analysisData).length > 0 ? */}
+                    {step < 1 ? (
+                      <>
+                        <Button
+                          onClick={handleAnalysisData}
+                          variant="contained"
+                          color="success"
+                          sx={{
+                            textTransform: "none",
+                            fontWeight: "bold",
+                            py: "0.85rem",
+                          }}
+                        >
+                          Run sector analysis
+                        </Button>
 
-                {/* right side */}
-                <Stack
-                    direction={'column'}
-                    spacing={8}
-                    sx={{
-                        bgcolor: 'white',
-                        height: '100%',
-                        // width: '40%',
-                        width: open ? '40%' : '0px',
-                        display: open ? 'flex' : 'none',
-                        p: 3,
-                        // left: 'auto',
-                        // right: '0',
-                        overflow: 'hidden',
-                        overflowY: 'auto',
-                        // transition: 'width 0.3s ease-out',
-                    }}
-                >
-                    <Stack direction={'row'} justifyContent={'flex-end'} sx={{ marginBottom: '-2.5rem !important' }}>
-                        {/* Object.keys(analysisData).length > 0 ? */}
-                        {
-                            step !== 0 ?
-                                <IconButton onClick={handleBackSidebar} sx={{ bgcolor: 'gray !important', color: '#ffffff' }}><ArrowBack /> </IconButton>
-                                :
-                                <IconButton onClick={handleCloseSidebar} sx={{ bgcolor: 'gray !important', color: '#ffffff' }}><Close /> </IconButton>
-                        }
-                    </Stack>
+                        <Button
+                          onClick={handleCountryData}
+                          variant="contained"
+                          color="success"
+                          sx={{
+                            textTransform: "none",
+                            fontWeight: "bold",
+                            py: "0.85rem",
+                          }}
+                        >
+                          Run country analysis
+                        </Button>
+                      </>
+                    ) : null}
+                  </Stack>
 
-                    {/* !Object.keys(analysisData).length > 0 ? */}
-                    {countryStep === 0 ?
-                        <>
-                            {step === 0 ?
-                                <>
-                                    <Stack direction={'row'}  alignItems={'center'} spacing={2}>
-                                        {selectedShortText?.split('')?.map((item, i) => (
-                                            <>
-                                                <Typography lineHeight={1} key={`${item}`} variant='h6' fontWeight={600} >
-                                                    {/* {item} */}
-                                                    {cubeDetails[item]?.title}
-                                                </Typography>
+                  <Stack direction={"column"} spacing={2}>
+                    {selectedShortText?.split("")?.map((item, i) => (
+                      <>{HTMLReactParser(cubeDetails[item]?.text)}</>
+                    ))}
+                  </Stack>
 
-                                                {i < (selectedShortText.length - 1) ? <Divider sx={{ borderWidth: "1px", height: "100%", borderColor: "gray" }} /> : null}
-                                            </>
-                                        ))}
-                                    </Stack>
-
-                                    <Stack direction={'column'} spacing={2}>
-                                        {selectedShortText?.split('')?.map((item, i) => (
-                                            <>
-                                            {HTMLReactParser(cubeDetails[item]?.text)}
-                                            </>
-                                        ))}
-                                    </Stack>
-                                                                     
-                                    {/* 
+                  {/* 
                                     <Stack direction={'row'} alignItems={'center'} spacing={2}>
                                         {selectedCubeData.length > 0 ? selectedCubeData.map((item, i) => (
                                             <>
@@ -219,46 +275,36 @@ const SecondLayerLayout = () => {
                                             </Typography>
                                         </Stack>
                                     )) : null} */}
-                                </> : null
-                            }
+                </>
+              ) : null}
 
-                            {/* ? analysisData.map((item, i) => ( */}
-                            {step === 2 ?
-                                (<Stack>
-                                    <Typography variant='h5' fontWeight={600} >
-                                        {analysisData.title}
-                                    </Typography>
-                                    <Typography variant='' fontWeight={500} >
-                                        {analysisData.overviews.details}
-                                    </Typography>
-                                </Stack>) : null}
-
-                            {step === 1 ? <SecondLayer
-                                setAnalysisData={setAnalysisData}
-                                selectedShortText={selectedShortText}
-                                onClickNext={handleSelectAnalysis}
-                            /> : null}
-
-
-                            <Stack direction={'row'} spacing={3} justifyContent={'flex-start'} sx={{ marginBottom: '' }}>
-                                {/* {!Object.keys(analysisData).length > 0 ? */}
-                                {step < 1 ?
-                                    <>
-                                        <Button onClick={handleAnalysisData} variant="contained" color="success" sx={{ textTransform: 'none', fontWeight: 'bold', py: '0.85rem' }} >Run sector analysis</Button>
-
-                                        <Button onClick={handleCountryData} variant="contained" color="success" sx={{ textTransform: 'none', fontWeight: 'bold', py: '0.85rem' }} >Run country analysis</Button>
-                                    </>
-                                    : null
-                                }
-                            </Stack>
-                        </>
-                        : <Typography textAlign={'center'}>Coming Soon...</Typography>}
-
-
+              {/* ? analysisData.map((item, i) => ( */}
+              {step === 2 ? (
+                <Stack>
+                  <Typography variant="h5" fontWeight={600}>
+                    {analysisData.title}
+                  </Typography>
+                  <Typography variant="" fontWeight={500}>
+                    {analysisData.overviews.details}
+                  </Typography>
                 </Stack>
-            </Stack>
-        </>
-    );
+              ) : null}
+
+              {step === 1 ? (
+                <SecondLayer
+                  setAnalysisData={setAnalysisData}
+                  selectedShortText={selectedShortText}
+                  onClickNext={handleSelectAnalysis}
+                />
+              ) : null}
+            </>
+          ) : (
+            <Typography textAlign={"center"}>Coming Soon...</Typography>
+          )}
+        </Stack>
+      </Stack>
+    </>
+  );
 };
 
 export default SecondLayerLayout;
